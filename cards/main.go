c0542f2beed67ced7b0a76e5064440f674403ef5
Chirag Gupta <chirag2309.gupta@gmail.com>
2026-03-15 23:51:52 +0530

    def var, func, & slice, for loop

---
+++ cards/main.go
@@ -0,0 +1,27 @@
+package main
+
+// functions in golang
+// func firstName() string {
+// 	return "Chirag"
+// }
+// func lastName() string {
+// 	return "Gupta"
+// }
+
+func main() {
+
+	// slice(arraylist) in golang
+	// names := []string{"Chirag", "Uma"}
+	// names = append(names, "Sudhanshu")
+	// fmt.Println(names)
+	// for i, name := range names {
+	// 	fmt.Println(i, name)
+	// }
+
+	// name := firstName() + " " + lastName()
+
+	// how to declare a variable in golang
+	// var name string = "Chirag Gupta"
+	// name := "Chirag Gupta"
+	// fmt.Println(name)
+}
