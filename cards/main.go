82340440073fa48cb00ada287b6d680d20621ee3
Chirag Gupta <chirag2309.gupta@gmail.com>
2026-03-25 15:14:39 +0530

    OO vs GO approach, Custom Type Declaration, Receiver Function, Slice Range, Multiple Return Values

--- cards/main.go
+++ cards/main.go
@@ -1,27 +1,8 @@
 package main
 
-// functions in golang
-// func firstName() string {
-// 	return "Chirag"
-// }
-// func lastName() string {
-// 	return "Gupta"
-// }
-
 func main() {
-
-	// slice(arraylist) in golang
-	// names := []string{"Chirag", "Uma"}
-	// names = append(names, "Sudhanshu")
-	// fmt.Println(names)
-	// for i, name := range names {
-	// 	fmt.Println(i, name)
-	// }
-
-	// name := firstName() + " " + lastName()
-
-	// how to declare a variable in golang
-	// var name string = "Chirag Gupta"
-	// name := "Chirag Gupta"
-	// fmt.Println(name)
+	cards := newDeck()
+	hand, remainingCards := deal(cards, 13)
+	hand.print()
+	remainingCards.print()
 }
